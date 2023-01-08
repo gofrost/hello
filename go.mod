f3cf703b986ee324495eb4eee57bdf0f391ab40f
Frog <lighthom@gmail.com>
2023-01-08 15:27:07 +0200

    first

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ser
+
+go 1.19
+
+require github.com/headfirstgo/keyboard v0.0.0-20170926053303-9930bcf72703
---
+++ main.go
@@ -0,0 +1,26 @@
+// pass_fail сообщает, сдал ли пользователь экзамен.
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/headfirstgo/keyboard"
+)
+
+func main() {
+	fmt.Print("Enter a grade: ")
+	grade, err := keyboard.GetFloat()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	var status string
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	fmt.Println("A grade of", grade, "is", status)
+
+}
